fd15733e79d53afdc3bb79279a33692b057de7a2
cys <cys107417>
2019-02-26 16:11:37 +0800

    build

---
+++ src/main/java/com/project/interceptor/AuthorizedInterceptor.java
@@ -0,0 +1,22 @@
+package com.project.interceptor;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 自定义拦截器
+ */
+public class AuthorizedInterceptor extends HandlerInterceptorAdapter {
+    /**
+     *访问前拦截
+     */
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("open");
+        return super.preHandle(request, response, handler);
+    }
+
+}
