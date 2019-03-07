3c53b7518bef709bfba1a59cd6c3e9ed2ef1fbf5
cys <cys107417>
2019-03-07 16:39:27 +0800

    build

--- src/main/java/com/project/controller/MainController.java
+++ src/main/java/com/project/controller/MainController.java
@@ -3,14 +3,16 @@ package com.project.controller;
 import com.project.config.SessionAttribute;
 import com.project.model.UserModel;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPReply;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
 @Controller
@@ -27,7 +29,26 @@ public class MainController {
    }
 
     @RequestMapping("addSession")
-    public void addSession(@ModelAttribute UserModel userModel, HttpServletRequest req)throws Exception{
+    public void addSession( @RequestHeader("Accept") String head, @CookieValue("NAME") String NAME, @ModelAttribute UserModel userModel, HttpServletRequest req)throws Exception{
       req.getSession().setAttribute("user",userModel);
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        FileInputStream inputStream =new FileInputStream("C:/Users/cys/Desktop/www.jpg");
+        FTPClient ftp = new FTPClient();
+        ftp.connect("47.95.245.138", 21);
+        ftp.login("107417", "107417");
+        ftp.setFileType(2);
+
+        int reply = ftp.getReplyCode();
+        if (!FTPReply.isPositiveCompletion(reply)) {
+            System.err.println("FTP服务器拒绝连接 ");
+            ftp.disconnect();
+        }
+        ftp.setControlEncoding("UTF-8");
+        boolean b = ftp.storeFile("www.jpg", inputStream);
+        System.out.println(b);
+        System.out.println(reply);
     }
 }
