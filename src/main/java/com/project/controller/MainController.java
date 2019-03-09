b0939271c3adc3efaa63058f4bff13c67cf2303f
iscys <iscys>
2019-03-09 14:04:39 +0800

    ftp

--- src/main/java/com/project/controller/MainController.java
+++ src/main/java/com/project/controller/MainController.java
@@ -3,17 +3,22 @@ package com.project.controller;
 import com.project.common.fileupload.FileUtils;
 import com.project.common.fileupload.FtpUpload;
 import com.project.common.model.FileModel;
-import com.project.model.UserModel;
-import org.apache.commons.net.ftp.FTPClient;
-import org.apache.commons.net.ftp.FTPReply;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.support.DefaultMultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.Iterator;
+import java.util.List;
 
 @Controller
 @RequestMapping("/main")
@@ -29,12 +34,26 @@ public class MainController {
 
     @RequestMapping("upload")
     public void upload(MultipartFile file, HttpServletRequest req)throws Exception{
+        DefaultMultipartHttpServletRequest request=(DefaultMultipartHttpServletRequest)req;
+        Iterator<String> fileNames = request.getFileNames();
+        MultiValueMap<String, MultipartFile> multiFileMap = request.getMultiFileMap();
+        MultipartFile file1 = multiFileMap.getFirst("file");
         InputStream fileStream = file.getInputStream();
         String filename = file.getOriginalFilename();
         String suff =filename.substring(filename.lastIndexOf(".")+1);
         FileModel fileModel = FtpUpload.getInstance().uploadFile(fileStream, suff, null);
         System.out.println(fileModel);
 
+
+        //--------传统fileupload 文件上传-----------//
+
+        DiskFileItemFactory fileItemFactory=new DiskFileItemFactory();
+        ServletFileUpload fileUpload =new ServletFileUpload(fileItemFactory);
+        List<FileItem> fileItems = fileUpload.parseRequest(req);
+        FileItem fileItem = fileItems.get(0);
+        String fieldName = fileItem.getFieldName();
+        System.out.println(fieldName);
+        long size = fileItem.getSize();
     }
 
 
